c97550c1a8aa7bfd151360b721fd582a61753d22
mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2025-04-20 17:57:09 +0900

    Update Args

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+use clap::{self, Parser};
 use rmcp::transport::sse_server::SseServer;
 mod functions;
 
-const BIND_ADDRESS: &str = "127.0.0.1:8000";
+
+#[derive(clap::Parser, Debug)]
+pub struct Args {
+    #[clap(long, default_value = "127.0.0.1", env = "MCP_STD_SERVER_HOST")]
+    pub host: String,
+
+    #[clap(short, long, default_value = "8000", env = "MCP_STD_SERVER_PORT")]
+    pub port: u16,
+}
 
 #[tokio::main]
 async fn main() {
-    let ct = SseServer::serve(BIND_ADDRESS.parse().unwrap())
+    let args = Args::parse();
+    let bind_address = format!("{}:{}", args.host, args.port);
+
+    let ct = SseServer::serve(bind_address.parse().unwrap())
         .await.unwrap()
         .with_service(functions::Functions::new);
 
